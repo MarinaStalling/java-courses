694c687c15c5936c1648b77b00be9fe2aab9b6ed
Marina Stalling <Marina Stalling>
2016-09-15 21:46:32 +0200

    1 version nicht gültig

--- Calculate.java
+++ Calculate.java
@@ -1,7 +1,62 @@
 public class Calculate {
 	
 	public static void main (String[] args){
-		System.out.println("Calculate...");
+
+		if (args[0].contains(".") || args[1].contains(",")){
+			double first = Double.valueOf(args[0].replace(",", "."));
+			double second = Double.valueOf(args[1].replace(",", "."));
+			System.out.println("Sum = " + getSum(first, second));
+			System.out.println("Dif = " + getDiff(first, second));
+			System.out.println("Mult = " + getMult(first, second));
+			System.out.println("Div = " + getDiv(first, second));
+			System.out.println("Exp = " + getExp(first, second));
+		}
+		else
+		{
+			int first = Integer.valueOf(args[0]);
+			int second = Integer.valueOf(args[1]);
+			System.out.println("Sum = " + getSum(first, second));
+			System.out.println("Dif = " + getDiff(first, second));
+			System.out.println("Mult = " + getMult(first, second));
+			System.out.println("Div = " + getDiv(first, second));
+			System.out.println("Exp = " + getExp(first, second));
+		}
+				
 		
+	}
+	
+	static int getSum(int first, int second){
+	    return first+second;
+	}
+	static double getSum(double first, double second){	         
+	    return first+second;
+	}
+	
+	static int getDiff(int first, int second){
+	    return first-second;
+	}
+	static double getDiff(double first, double second){	         
+	    return first-second;
+	    
+	}
+	static int getMult(int first, int second){
+	    return first*second;
+	}
+	static double getMult(double first, double second){	         
+	    return first*second;
+	}
+	
+	static int getDiv(int first, int second){
+	    return first/second;
+	}
+	static double getDiv(double first, double second){	         
+	    return first/second;
+	}
+	
+	static int getExp(double first, double second){
+	    return Math.getExponent(Math.pow(first, second));
+	}
+	static double getExp(int first, int second){	         
+	    return Math.pow(first,second);
 	}
 }
